5e97304667dca94a6b4be38c17d9b233d554e20d
eccheandre <Andrea@Andrea-2.local>
2017-12-14 12:44:20 +0100

    Small modifications

--- readfiles.R
+++ readfiles.R
@@ -38,31 +38,21 @@ tail(raw_data)
 
 # Creating date column
 
-# The data table way
-# Still needs some improvements
-intermediate <<- raw_data[,date:=paste0(year,"-",month,"-",day)]
-
-h_data <<- intermediate[,c("year","month","day"):=NULL]
-
+raw_data$year <- as.character(raw_data$year); raw_data$month <- as.character(raw_data$month); raw_data$day <- as.character(raw_data$day)
+date_column <- data.frame(ob_date = as.Date(paste(raw_data$year, raw_data$month, raw_data$day, sep='-')))
+h_data <- data.table(cbind(date_column, raw_data))
+date_column <- NULL
 head(h_data)
-tail(h_data)
-
-#ALTERNATIVE WAY:
-# h_data$year <- as.character(h_data$year); h_data$month <- as.character(h_data$month); h_data$day <- as.character(h_data$day)
-# dateddf <- h_data.frame(dated = as.Date(paste(h_data$year, h_data$month, h_data$day, sep='-')))
-# h_data <- cbind(dateddf, h_data)
-# dateddf <- NULL
-# head(h_data)
-# h_data$year <- NULL
-# h_data$month <- NULL
-# h_data$day <- NULL
+h_data$year <- NULL
+h_data$month <- NULL
+h_data$day <- NULL
 
 # ===================================================
 
 # Subsetting raw_data to create a daily dataset and merge it with weather info and parameter name
-daily_data <- h_data[,.(daily_avg=mean(value)), by=.(date,station,parameter)]
+daily_data <- h_data[,.(daily_avg=mean(value)), by=.(ob_date,station,parameter)]
 
-daily_data <- merge(daily_data, weather, by.daily_data=)
+daily_data_merged <- merge(daily_data, weather, by.x="ob_date", by.y="date", all=FALSE)
 
 
 
