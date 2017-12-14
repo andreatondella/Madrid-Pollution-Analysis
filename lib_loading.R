9461db763e5afaeba0a44596375b225cac052634
Eccheandre <tondella.andrea@gmail.com>
2017-12-14 23:01:12 +0100

    small changes

--- lib_loading.R
+++ lib_loading.R
@@ -1,6 +1,6 @@
 #Srcipt where to load all the libraries needed for the project
 
-library(data.table)
-library(readxl)
+library(data.table) #package to deal with data tables
+library(readxl) #package to read excel files
 #library(openair)
 #library(dygraphs)
