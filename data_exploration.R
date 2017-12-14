d2c37ca340d05451f8fc9b3171787662b96709c9
eccheandre <Andrea@Andrea-2.local>
2017-12-14 10:53:50 +0100

    Script for exploring the data

---
+++ data_exploration.R
@@ -0,0 +1,24 @@
+# MBDO2-2
+# R Group Assignment
+# ===================================================
+
+# Exploring the datasets
+
+# ===================================================
+
+#Loading libraries
+source("lib_loading.R")
+
+# ===================================================
+
+# List of unique stations/parameters
+stationlist <- unique(h_data$station)
+paramlist <- unique(h_data$parameter)
+
+# ===================================================
+
+# Parameter per station
+for(x in stationlist) {
+  print(x)
+  print(h_data[station == x, unique(parameter)])
+}
