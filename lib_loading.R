73bc888e1301021c3472df28a40ed6e30b69dcfc
Eccheandre <tondella.andrea@gmail.com>
2017-12-16 17:37:45 +0100

    Some libraries added

--- lib_loading.R
+++ lib_loading.R
@@ -3,5 +3,7 @@
 library(data.table) #package to deal with data tables
 library(readxl) #package to read excel files
 library(tidyr) #package to reshape data
+library(ggplot2)
+library(gridExtra)
 #library(openair)
 #library(dygraphs)
