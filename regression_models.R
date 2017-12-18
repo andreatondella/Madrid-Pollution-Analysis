5c926c87805efa6f38ef0a2f342a1175fee875ae
Deodatta Jagtap <deodattaj@CYCLOPEAN-MacBook-Pro.local>
2017-12-18 09:30:11 +0100

    New file to add and analyse regression models.

---
+++ regression_models.R
@@ -0,0 +1,12 @@
+# MBDO2-2
+# R Group Assignment
+# ===================================================
+
+# Building regression models on data subsets
+
+# ===================================================
+
+#Loading libraries
+source("lib_loading.R")
+
+# ===================================================
